3d44adaee7c18980b183f93f3a68b8a5380770fc
M <65395420+micha-01@users.noreply.github.com>
2024-04-22 15:50:46 +0200

    reworked graph structure: nodes are ints starting from 0

--- graph.py
+++ graph.py
@@ -9,22 +9,27 @@ tasks = read_tasks_csv(Path("tasks.csv"))
 
 
 
-def tasks_to_bipartite(tasks: list[Task],
-                       weight: int | None = None) -> nx.Graph:
+def tasks_to_bipartite(tasks: list[Task]) -> nx.Graph:
     """
     creates a bipartite graph from a list of tasks
     cannot use nx.algorithms.bipartite because the graph can be disconnected
     """
     G = nx.Graph()
 
-    for i, t in enumerate(tasks):
-        # add a node for each 1h segment of a task
-        # t13 := 3rd segment of task 1
-        G.add_nodes_from([f"t{i}_{x}" for x in range(t['duration'])])
+    # find the starting index for v_j in V
+    idx_v: int = sum(map(lambda task: task['duration'], tasks))
 
-        # add node for each work hour of the day, i.e. 08:00 to 18:00
-        G.add_nodes_from([i for i in range(8, 19)])
+    # u_i in U: i in {0, ..., idx_v - 1}
+    # add a node for each 1h segment of a task
+    # t13 := 3rd segment of task 1
+    G.add_nodes_from([i for i in range(idx_v)])
 
+    # v_j in V: j in {idx_v, ..., idx_v + 8}
+    # add node for each work hour of the day, i.e. 08:00 to 18:00
+    G.add_nodes_from([i for i in range(idx_v, idx_v + 10)])
+
+    i: int = 0
+    for t in tasks:
         # add edge iff task can be done in this hour segment,
         # i.e. is between the start time and the end time of the task
         # e.g. start: 08:00, end: 10:00 -> edge only to 8 and 9 (NOT 10)
@@ -32,11 +37,12 @@ def tasks_to_bipartite(tasks: list[Task],
         end = t['end']
         j: int = 0
         while (j < t['duration']):
-            G.add_edge(f"t{i}_{j}", start.hour, weight=t['prio'])
+            G.add_edge(i + j, start.hour - idx_v, weight=t['prio'])
             start += timedelta(hours=1)
             if start >= end:
                 start = t['start']
                 j += 1
+        i += j
     return G
 
 
